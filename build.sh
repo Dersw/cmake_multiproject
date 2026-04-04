ad9c08a425409d3e95e4f29d1fad6057aefe37ef
Dersw <htno2007@gmail.com>
2026-04-04 10:34:03 +0300

    [root] update build and CMake scripts for vector and doubly linked list projects

--- build.sh
+++ build.sh
@@ -16,13 +16,17 @@ arr_files[0]="run_bubble_sort.sh"
 arr_files[1]="run_bubble_sort_test.sh"
 arr_files[2]="run_multilevel_menu.sh"
 arr_files[3]="run_long_numbers_test.sh"
+arr_files[4]="run_double_linked_list_test.sh"
+arr_files[5]="run_vector_test.sh"
 
 arr_folders[0]="bubble_sort"
 arr_folders[1]="bubble_sort_test/test"
 arr_folders[2]="multilevel_menu"
 arr_folders[3]="long_numbers/test"
+arr_folders[4]="double_linked_list/test"
+arr_folders[5]="vector/test"
 
-for i in {0..3}; do
+for i in {0..5}; do
     cp "../$SOURCE_FOLDER/${arr_folders[i]}/${arr_files[i]}" "./${arr_folders[i]}"
 done
 
